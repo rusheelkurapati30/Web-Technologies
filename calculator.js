c44427eaa6131aa8663203d0fcec937eac0ffb37
rusheelkurapati30 <62334199+rusheelkurapati30@users.noreply.github.com>
2020-11-08 18:42:59 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,158 @@
+var btn = document.createElement("button");
+function myFunction(number)
+{
+	btn.innerHtml = calculator.display.value+=number;
+}
+function myOperation(operator)
+{
+	btn.innerHtml = calculator.display.value+=operator;
+}
+function result()
+{
+	btn.innerHtml = calculator.display.value=eval(calculator.display.value);
+}
+// function eight()
+// {
+// 	btn.innerHtml = calculator.display.value+='8';
+// }
+// function nine()
+// {
+// 	btn.innerHtml = calculator.display.value+='9';
+// }
+// function multiplication()
+// {
+// 	btn.innerHtml = calculator.display.value+='*';
+// }
+// function four()
+// {
+// 	btn.innerHtml = calculator.display.value+='4';
+// }
+// function five()
+// {
+// 	btn.innerHtml = calculator.display.value+='5';
+// }
+// function six()
+// {
+// 	btn.innerHtml = calculator.display.value+='6';
+// }
+// function subtraction()
+// {
+// 	btn.innerHtml = calculator.display.value+='-';
+// }
+// function one()
+// {
+// 	btn.innerHtml = calculator.display.value+='1';
+// }
+// function two()
+// {
+// 	btn.innerHtml = calculator.display.value+='2';
+// }
+// function three()
+// {
+// 	btn.innerHtml = calculator.display.value+='3';
+// }
+// function addition()
+// {
+// 	btn.innerHtml = calculator.display.value+='+';
+// }
+// function clear()
+// {
+// 	btn.innerHtml = calculator.display.value+='';
+// }
+// function zero()
+// {
+// 	btn.innerHtml = calculator.display.value+='0';
+// }
+// function result()
+// {
+// 	btn.innerHtml = calculator.display.value=eval(calculator.display.value);
+// }
+// function division()
+// {
+// 	btn.innerHtml = calculator.display.value+='1';
+// }
+
+
+// var btn = document.createElement("button");
+// function seven()
+// {
+// 	// btn.type = "button"; 
+// 	btn.innerHtml = calculator.display.value+='7';
+// 	// btn.setAttribute("type", "button");
+// 	// btn.setAttribute("onclick", "btn");
+// }
+// function eight()
+// {
+// 	btn.innerHtml = calculator.display.value+='8';
+// 	// btn.setAttribute("onclick", "btn");
+// }
+// function nine()
+// {
+// 	btn.innerHtml = calculator.display.value+='9';
+// 	btn.setAttribute("onclick", "btn");
+// }
+// function multiplication()
+// {
+// 	btn.innerHtml = calculator.display.value+='*';
+// 	btn.setAttribute("onclick", "btn");
+// }
+// function four()
+// {
+// 	btn.innerHtml = calculator.display.value+='4';
+// 	btn.setAttribute("onclick", "btn");
+// }
+// function five()
+// {
+// 	btn.innerHtml = calculator.display.value+='5';
+// 	btn.setAttribute("onclick", "btn");
+// }
+// function six()
+// {
+// 	btn.innerHtml = calculator.display.value+='6';
+// 	btn.setAttribute("onclick", "btn");
+// }
+// function subtraction()
+// {
+// 	btn.innerHtml = calculator.display.value+='-';
+// 	btn.setAttribute("onclick", "btn");
+// }
+// function one()
+// {
+// 	btn.innerHtml = calculator.display.value+='1';
+// 	btn.setAttribute("onclick", "btn");
+// }
+// function two()
+// {
+// 	btn.innerHtml = calculator.display.value+='2';
+// 	btn.setAttribute("onclick", "btn");
+// }
+// function three()
+// {
+// 	btn.innerHtml = calculator.display.value+='3';
+// 	btn.setAttribute("onclick", "btn");
+// }
+// function addition()
+// {
+// 	btn.innerHtml = calculator.display.value+='+';
+// 	btn.setAttribute("onclick", "btn");
+// }
+// function clear()
+// {
+// 	btn.innerHtml = calculator.display.value+='';
+// 	btn.setAttribute("onclick", "btn");
+// }
+// function zero()
+// {
+// 	btn.innerHtml = calculator.display.value+='0';
+// 	btn.setAttribute("onclick", "btn");
+// }
+// function result()
+// {
+// 	btn.innerHtml = calculator.display.value=eval(calculator.display.value);
+// 	btn.setAttribute("onclick", "btn");
+// }
+// function division()
+// {
+// 	btn.innerHtml = calculator.display.value+='1';
+// 	btn.setAttribute("onclick", "btn");
+// }
---
+++ practice.js
@@ -0,0 +1,10 @@
+var btn = document.createElement("button");
+var symbol;
+var button;
+function generateButton()
+{
+	for(symbol = 0; symbol < 10; symbol++)
+	{
+		document.getElement("button").innerHTML = symbol;
+	}
+}
